0836ded6b73ed16fe0ba1af5cd5e8ccd7e499f76
florianZac <florianaizac@msn.com>
2026-03-19 17:46:03 +0100

    modif

--- src/Controller/TestEmailController.php
+++ src/Controller/TestEmailController.php
@@ -8,7 +8,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Service\MailerService;
 use App\Entity\Utilisateur;
-use OpenApi\Annotations as OA;
 
 #[Route('')]
 final class TestEmailController extends AbstractController
@@ -18,14 +17,6 @@ final class TestEmailController extends AbstractController
    * @param mailerService $MailerService Le service mail
    * @return JsonResponse
    */
-  #[Route('/test-email', name: 'api_test_email', methods: ['POST'])]
-  #[OA\Post(
-      summary: 'Envoyer un email de test',
-      description: 'Envoie un email de bienvenue à un utilisateur factice pour tester le mailer.',
-  )]
-  #[OA\Tag(name: 'Admin - Test Email')]
-  #[OA\Response(response: 200, description: 'Email de test envoyé !')]
-  #[OA\Response(response: 400, description: 'Email manquant ou invalide')]
   public function sendTestEmail(MailerService $mailerService): JsonResponse
   {
     // Récupère l'email depuis le POST JSON si fourni
