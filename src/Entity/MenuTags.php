ee89222c8c846834f62fbc7287eab907e5798d3b
florianZac <florianaizac@msn.com>
2026-03-27 04:44:04 +0100

    Correction de bug

--- src/Entity/MenuTags.php
+++ src/Entity/MenuTags.php
@@ -12,53 +12,56 @@ use App\Entity\Menu;
 #[ORM\Entity(repositoryClass: MenuTagsRepository::class)]
 class MenuTags
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column]
+  private ?int $id = null;
 
-    #[ORM\Column(length: 100, unique: true)]
-    private ?string $tag = null;
+  #[ORM\Column(length: 100, unique: true)]
+  private ?string $tag = null;
 
-    #[ORM\ManyToMany(targetEntity: Menu::class, mappedBy: 'tags')]
-    private Collection $menus;
+  #[ORM\ManyToMany(targetEntity: Menu::class, mappedBy: 'tags')]
+  private Collection $menus;
 
-    public function __construct()
-    {
-        $this->menus = new ArrayCollection();
+  public function __construct()
+  {
+    $this->menus = new ArrayCollection();
+  }
+
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
+
+  public function getTag(): ?string
+  {
+    return $this->tag;
+  }
+
+  public function setTag(string $tag): static
+  {
+    $this->tag = $tag;
+    return $this;
+  }
+  public function getMenus(): Collection
+  {
+    return $this->menus;
+  }
+
+  public function addMenu(Menu $menu): static
+  {
+    if (!$this->menus->contains($menu)) {
+      $this->menus->add($menu);
+      $menu->addTag($this);
     }
+    return $this;
+  }
 
-    public function getId(): ?int
-    {
-        return $this->id;
+  public function removeMenu(Menu $menu): static
+  {
+    if ($this->menus->removeElement($menu)) {
+      $menu->removeTag($this);
     }
-
-    public function getTag(): ?string
-    {
-        return $this->tag;
-    }
-
-    public function setTag(string $tag): static
-    {
-        $this->tag = $tag;
-        return $this;
-    }
-    public function getMenus(): Collection
-    {
-        return $this->menus;
-    }
-
-    public function addMenu(Menu $menu): static
-    {
-        if (!$this->menus->contains($menu)) {
-            $this->menus->add($menu);
-        }
-        return $this;
-    }
-
-    public function removeMenu(Menu $menu): static
-    {
-        $this->menus->removeElement($menu);
-        return $this;
-    }
+    return $this;
+  }
 }
